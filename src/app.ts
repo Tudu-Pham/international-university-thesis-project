5e8ea60debf55f985d172b27d193abcebb0acf5f
dangnm-2032 <dangnm.working@gmail.com>
2026-04-14 20:46:47 +0700

    fix: video player

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+app.use("/videos", express.static("src/videos"));
 
 webRoutes(app);
 
